c0aea6e91a2b966c8f61485cf0e67edf80ca340c
Jonathan Beebout <jlbeebout@gmail.com>
2025-01-25 17:16:57 -0800

    Initial Commit

---
+++ NooroTakeHome/NooroTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NooroTakeHomeApp.swift
+//  NooroTakeHome
+//
+//  Created by Jonathan on 1/25/25.
+//
+
+import SwiftUI
+
+@main
+struct NooroTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NooroTakeHomeTests/NooroTakeHomeTests.swift
@@ -0,0 +1,17 @@
+//
+//  NooroTakeHomeTests.swift
+//  NooroTakeHomeTests
+//
+//  Created by Jonathan on 1/25/25.
+//
+
+import Testing
+@testable import NooroTakeHome
+
+struct NooroTakeHomeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
